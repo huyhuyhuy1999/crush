09c06cb899b3b838528d23a0c02397b269e2d842
Aker <80908827+huyhuyhuy1999@users.noreply.github.com>
2021-11-15 23:25:55 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,19 @@
+const CONFIGGG = {
+    introTitle: 'Mai à!',
+    introDesc: `Anh đây không biết làm thơ
+    Yêu bằng tình cảm chứ không thơ tình
+    Vậy nên Mai có thích mình
+    Nhanh tay inbox mình thành tình yêu ❤`,
+    btnIntro: 'hihi',
+    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    btnYes: 'Yêu anh',
+    btnNo: 'Không, tks!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Biết mà, tui cũng iu Mai nhìu lắm',
+    messDesc: 'Inbox trò chuyện tâm sự đêm phia ik.',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.facebook.com/Huy.sieucapvjppro' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
